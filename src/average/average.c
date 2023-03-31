0c68c8f8f3a081f7161dc0bc151264af3e05c6e2
Imanol Gomez <imanol@steinergomez.com>
2023-03-31 14:19:16 +0200

    added semicolon

--- src/average/average.c
+++ src/average/average.c
@@ -4,9 +4,9 @@ float average(float arr[], unsigned int size)
 {
     if (size == 0)
     {
-        return 0.0
+        return 0.0;
     }
-    
+
     float total = 0;
     for(unsigned int i = 0; i < size; i++){
         total+=arr[i];
